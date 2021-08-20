1bd91a69eca645adbe655868bbb3fdbcbc6355a5
LinaLu <lina.my.lu@gmail.com>
2021-08-20 16:38:32 +0200

    persist state when refreshing page

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,10 +2,16 @@ import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import {gameReducer} from './reducers';
 import {composeWithDevTools} from 'redux-devtools-extension';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
 
-export const configureStore = () =>
-  createStore(
-    gameReducer,
-    composeWithDevTools(applyMiddleware(logger)),
-  );
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, gameReducer)
+
+export const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(logger)))
+export const persistor = persistStore(store)
